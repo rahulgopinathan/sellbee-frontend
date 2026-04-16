253d37dc97faa8d41587c2ab21d0409cd21817d0
Rahul Gopinathan <rahulthettikulam@gmail.com>
2026-04-16 15:50:10 +0100

    Add Email and password states. Enable login feature

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -1,6 +1,19 @@
 import "./LoginForm.css";
+import { useState } from "react";
+import axios from "axios";
 
 function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleLoginClick() {
+    const response = await axios.post("http://localhost:3001/auth/signin", {
+      email: "john.doe1@example.com",
+      password: "password123",
+    });
+    console.log(response);
+  }
+
   return (
     <div className="login-form-container">
       <div className="app-icon">
@@ -11,17 +24,23 @@ function LoginForm() {
       <div className="user-creds">
         <input
           type="text"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
           className="user-email-input"
           placeholder="University Email"
         />
         <input
           className="user-password-input"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
           type="password"
           placeholder="Enter Password"
         />
       </div>
 
-      <button className="login-btn">Proceed</button>
+      <button onClick={handleLoginClick} className="login-btn">
+        Proceed
+      </button>
 
       <a
         className="terms-and-conditions-link"
